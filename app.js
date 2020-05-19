ed8ba7ff75d00e56d334ddd7a10b07c805caed5f
alex hill <hill.alexandermarshall@gmail.com>
2020-05-19 15:06:43 -0600

    May 19th Commit, up to basic stage

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors =require('cors'); 
 require('dotenv').config();
 var eventschedulerRouter = require('./routes/api/eventscheduler');
 var indexRouter = require('./routes/index');
@@ -19,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 app.use('/api/eventscheduler', eventschedulerRouter);
 app.use('/users', usersRouter);
 
